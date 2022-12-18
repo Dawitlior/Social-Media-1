3b7fe7446b2cda8666c95cf709a90f4194fbf3b9
Dawitlior <dawitlior777@gmail.com>
2022-12-18 22:22:03 +0200

    add

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
+
+function App() {
+  return <div className="App">
+    <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/profile/:userId" element={<ProfilePage />} />
+     </Routes>
+    </BrowserRouter>
+  </div>;
+}
+
+export default App;
