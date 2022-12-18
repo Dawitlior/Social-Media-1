688da3973a05239c1a3e1728ec1aef9992c20660
Dawitlior <dawitlior777@gmail.com>
2022-12-19 01:40:52 +0200

    add

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -19,7 +19,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
   const main = palette.neutral.main;
   const medium = palette.neutral.medium;
 
-  const isFriend = friends.find((friend) => friend._id === friendId);
+  const isFriend = friends.find((friend) => friend._id === friendId );
 
   const patchFriend = async () => {
     const response = await fetch(
