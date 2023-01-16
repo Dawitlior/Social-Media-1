6ce2b31d5c92908ead87104a6921499498aeff64
Dawitlior <dawitlior777@gmail.com>
2023-01-16 16:42:07 +0200

    add

--- client/src/App.js
+++ client/src/App.js
@@ -13,18 +13,26 @@ function App() {
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   const isAuth = Boolean(useSelector((state) => state.token));
 
-  return <div className="App">
-    <BrowserRouter>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-     <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route path="/home" element={isAuth ? <HomePage /> : <Navigate to="/" />} />
-      <Route path="/profile/:userId" element={isAuth ?<ProfilePage /> : <Navigate to="/" />} />
-     </Routes>
-    </ThemeProvider>
-    </BrowserRouter>
-  </div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -31,15 +31,15 @@ const Navbar = () => {
   const navigate = useNavigate();
   const user = useSelector((state) => state.user);
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
-
   const theme = useTheme();
   const neutralLight = theme.palette.neutral.light;
   const dark = theme.palette.neutral.dark;
   const background = theme.palette.background.default;
   const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
-
   const fullName = `${user.firstName} ${user.lastName}`;
+
+
 
   return (
     <FlexBetween padding="1rem 6%" backgroundColor={alt}>
@@ -72,7 +72,6 @@ const Navbar = () => {
           </FlexBetween>
         )}
       </FlexBetween>
-
       {/* DESKTOP NAV */}
       {isNonMobileScreens ? (
         <FlexBetween gap="2rem">
@@ -112,13 +111,10 @@ const Navbar = () => {
           </FormControl>
         </FlexBetween>
       ) : (
-        <IconButton
-          onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
-        >
+        <IconButton onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)} >
           <Menu />
         </IconButton>
       )}
-
       {/* MOBILE NAV */}
       {!isNonMobileScreens && isMobileMenuToggled && (
         <Box
